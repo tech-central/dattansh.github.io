57057ac1dd694eecc27a430bc2c8419991b2394a
jai135g@gmail.com <jai135g@gmail.com>
2021-11-25 12:43:26 +0530

    fds

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@ License: https://themeforest.net/licenses/standard
 		}
 		
 		if(body.hasClass('mobile')) {
-			$('.video-wrapper, .player').css('display', 'block');	
+			$('.video-wrapper, .player').css('display', 'none');	
 		}
 		
 		// GMap Background
